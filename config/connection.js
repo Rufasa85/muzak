1645d7cf57ad50cfd725f14e9c341ff802ca8be7
Joe Rehfuss <joe.rehfuss@gmail.com>
2021-04-23 14:33:24 -0700

    adding heorku config

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,23 @@
 const mysql = require("mysql");
+let connection
+if(process.env.JAWSDB_URL){
+    connection= mysql.createConnection(process.env.JAWSDB_URL);
+}else {
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-
-    // Your port; if not 3306
-    port: 3306,
-
-    // Your username
-    user: 'root',
-
-    // Your password
-    password: 'password',
-    database: 'top_songsDB',
-});
+    connection = mysql.createConnection({
+        host: 'localhost',
+        
+        // Your port; if not 3306
+        port: 3306,
+        
+        // Your username
+        user: 'root',
+        
+        // Your password
+        password: 'password',
+        database: 'top_songsDB',
+    });
+}
 
 connection.connect((err) => {
     if (err) throw err;
